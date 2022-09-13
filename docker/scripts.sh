312fa28d161d81ecd3edec52fccd2351514f27ea
James Johnson <xhunter0228@gmail.com>
2022-09-13 16:57:27 -0500

    update
    Co-authored-by: ben07ab <bjmslaughter@gmail.com>

--- docker/scripts.sh
+++ docker/scripts.sh
@@ -1,1 +1,2 @@
 docker build -t rdf-postgres -f Dockerfile-postgres ../ && docker run --rm -p 5432:5432 rdf-postgres
+docker build -t rdf-postgres -f Dockerfile-postgres ../ && docker run --rm -p 5434:5434 rdf-postgres
